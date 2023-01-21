d4e60b5322cb7fc79626915b5aae75866bdec581
Andy Newhouse <hi@andymnewhouse.me>
2023-01-21 15:48:15 -0600

    Add custom colors to dates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,25 @@
 module.exports = {
-  content: [
-    './resources/**/*.antlers.html',
-    './resources/**/*.blade.php',
-    './resources/**/*.vue',
-    './content/**/*.md'
-  ],
-  theme: {
-    extend: {
-        boxShadow: {
-            '3xl': '0 25px 28px -26px rgba(0,0,0,0.50), 0 16px 50px 1px rgba(0,0,0,0.11)',
-        }
+    content: [
+        './resources/**/*.antlers.html',
+        './resources/**/*.blade.php',
+        './resources/**/*.vue',
+        './content/**/*.md'
+    ],
+    theme: {
+        extend: {
+            boxShadow: {
+                '3xl': '0 25px 28px -26px rgba(0,0,0,0.50), 0 16px 50px 1px rgba(0,0,0,0.11)',
+            },
+            colors: {
+                custom: {
+                    blue: '#6bc4ff',
+                    green: '#6bffae',
+                    purple: '#9f6bff',
+                },
+            },
+        },
     },
-  },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
+    plugins: [
+        require('@tailwindcss/typography'),
+    ],
 }
