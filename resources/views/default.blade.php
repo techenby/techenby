67c82c335d46e541fcac25b3e43baea658bcf618
Andy Newhouse <hi@andymnewhouse.me>
2024-03-27 17:02:13 -0500

    Just Keep Swimming 🐠

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -1,9 +1,18 @@
 <x-layout :title="$title">
-    <x-container>
+    <x-container class="max-w-prose">
         <article class="px-4">
             <h1 class="text-5xl font-black max-w-xl mb-12">{{ $title }}</h1>
 
-            <x-card :image="$page->header_image" :content="$content" />
+            <div class="block bg-white rounded-lg shadow-lg dark:bg-mardi-950 overflow-hidden">
+                @if ($page->photo)
+                <img src="{{ url($page->photo->url) }}"
+                    alt="{{ $page->photo->alt }}"
+                    class="w-full"
+                />
+                @endif
+
+                <x-content class="p-8" :content="$page->content ?? $page->caption" />
+            </div>
         </article>
     </x-container>
 </x-layout>
