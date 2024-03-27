02631d006d1ba510038c85c6ae4f6e2015edf0db
Andy Newhouse <hi@andymnewhouse.me>
2024-03-27 12:05:31 -0500

    Move tags.show to blade

---
+++ resources/views/posts/tags/show.blade.php
@@ -0,0 +1,14 @@
+<x-layout title="Tags">
+    <x-container class="max-w-xl">
+        <h1 class="text-4xl lg:text-5xl font-black flex items-center space-x-4 mb-12">
+            <x-ant.svg :src="$icon" :aria-label="$title" role="img" class="size-12" />
+            <span>{{ $title }}</span>
+        </h1>
+
+        <div class="space-y-16">
+            @foreach($page->entries as $entry)
+                <x-card :image="$entry->photo" :content="$entry->caption" />
+            @endforeach
+        </div>
+    </x-container>
+</x-layout>
