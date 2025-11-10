4e36815288e55bfead221059d27c0992f1541902
Andy Newhouse <andy@techenby.com>
2025-11-10 11:35:49 -0600

    Rename global to brand

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,14 @@
 <x-layout :title="$title">
     <section class="max-w-lg mx-auto py-8 px-4
         lg:py-12 lg:px-0 lg:max-w-none lg:flex lg:items-center lg:justify-center lg:space-x-12">
-        <x-avatar :avatar="$avatar" />
+        <x-avatar :avatar="$brand->avatar" />
         <div class="mt-8 lg:mt-0 max-w-prose">
             <h1 class="text-xl sm:text-3xl font-black align-baseline">
-                {!! $heading !!}
+                {!! $brand->heading !!}
                 <x-ant.svg src="assets/logo" class="h-8 inline" alt="TechEnby Logo" />
             </h1>
             <div class="mt-4 prose lg:prose-xl dark:prose-invert prose-brand">
-                {!! $bio !!}
+                {!! $brand->bio !!}
             </div>
         </div>
     </section>
