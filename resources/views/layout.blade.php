8c2f1ea2f837f3b717e796a68788622d343b0910
Andy Newhouse <andy@techenby.com>
2026-05-04 23:32:33 -0500

    Add coming soon page

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,15 +1,18 @@
 <!doctype html>
-<html lang="{{ Statamic\Facades\Site::current()->shortLocale() }}">
+<html lang="{{ Statamic\Facades\Site::current()->shortLocale() }}" class="antialiased">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>{{ $title ?? Statamic\Facades\Site::current()->name() }}</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&family=Figtree:wght@300..900&family=Geist:wght@100..900&family=Geist+Mono:wght@100..900&family=IBM+Plex+Mono:wght@400;500;600;700&family=Instrument+Serif:ital@0;1&family=Lora:wght@400..700&family=Press+Start+2P&family=VT323&display=swap" rel="stylesheet">
         @vite(['resources/js/site.js', 'resources/css/site.css'])
     </head>
-    <body class="bg-zinc-100 dark:bg-zinc-900 font-sans leading-normal text-zinc-800 dark:text-zinc-400">
-        <div class="mx-auto px-2 lg:min-h-screen flex flex-col items-center justify-center">
+    <body class="font-sans">
+        <main id="app" class="isolate">
             @yield('content')
-        </div>
+        </main>
     </body>
 </html>
